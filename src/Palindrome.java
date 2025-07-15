1cdcfdb01da12c86e4e5b28d377e90be437cb8bc
Rimjhimm12 <rimjhimmallick6@gmail.com>
2025-07-14 21:44:15 -0500

    Updated Palindrome function
    1. Checking palindrome for numbers and strings in separate functions.
    2. Method Overloading concept has been used.

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,26 +1,54 @@
+import java.util.Arrays;
+
 public class Palindrome {
-    public Boolean isPalindrome(int x) {
-        int originalNum = x;
-        int revarse = 0;
-        if(x>0) {
-            while (x != 0) {
-            int temp = x % 10;
-            revarse = revarse * 10 + temp;
-            x = x / 10;
-            }
-            if(originalNum == revarse)
-                return  true;
-            else
-                return false;
+    public boolean isPlindrome(int number){
+        boolean flag = false;
+
+        int original = number;
+        int result = 0;
+        while(number != 0) {
+            int temp = number % 10;
+            result = result * 10 + temp;
+            number = number / 10;
         }
-        else
-            return  false;
-
+        if(result == original){
+            System.out.println("Number is pelindrome");
+            flag = true;
+            return flag;
+        }else {
+            System.out.println("Given number is not pelindrome");
+            return flag;
+        }
     }
+    public boolean isPlindrome(String str){
 
+        String strNew = str.toLowerCase();
+        char ch[] = strNew.toCharArray();
+        char arr[] = new char[ch.length];
+        int j = 0;
+        for(int i = ch.length-1; i>=0; i--) {
+            arr[j] = ch[i];
+            j++;
+        }
+        String originalStr =  new String(arr);
+        if(originalStr.equals(strNew)){
+            System.out.println("Given String is Palindrome");
+            return true;
+        }else{
+            System.out.println("Given String is not a Palindrome");
+            return false;
+        }
+    }
     public static void main(String[] args) {
         Palindrome obj = new Palindrome();
-        System.out.println(obj.isPalindrome(-12041));
+        System.out.println(obj.isPlindrome(1102011));
+        System.out.println(obj.isPlindrome("maow"));
+
+
+
+
+
+
 
 
     }
