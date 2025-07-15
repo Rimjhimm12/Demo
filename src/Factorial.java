f3050bdb31d52a42bfcd769809b83665f33940e7
Rimjhim Mallick <82497904+Rimjhimm12@users.noreply.github.com>
2025-07-15 13:12:36 -0500

    Update Factorial.java

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,9 +1,14 @@
 public class Factorial{
-    public static void main(String[] args) {
-        int number = 7;
+    public int getFactorial(int number){
+        int original = number;
         for(int i =number-1; i>=1; i--){
             number = number*i;
         }
-        System.out.println("factorial is "+number);
+        return number;
+    }
+    public static void main(String[] args) {
+        Factorial obj = new Factorial();
+    
+        System.out.println("Factorial of "+number+" is "+obj.getFactorial(7));
     }
 }
